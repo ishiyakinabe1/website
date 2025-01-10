fcc124073086233637faa0e6ce1882492b261a25
ishiyakinabe1 <187490229+ishiyakinabe1@users.noreply.github.com>
2025-01-10 12:50:04 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     lazyImages.forEach(img => observer.observe(img));
 });
+function showImage(element) {
+    var modal = document.getElementById("modal");
+    var modalImage = document.getElementById("modalImage");
+    var captionText = document.getElementById("caption");
+
+    modal.style.display = "block";
+    modalImage.src = element.src;
+    captionText.innerHTML = element.alt;
+}
+
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
